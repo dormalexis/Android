ba3380a695473478796a9ae1184c962427c5d8ed
dormalexis <47395991+dormalexis@users.noreply.github.com>
2019-11-28 14:23:50 +0100

    Changement de fragments via un bouton

--- app/src/main/java/com/example/smartcity/LogInFragment.java
+++ app/src/main/java/com/example/smartcity/LogInFragment.java
@@ -1,19 +1,67 @@
 package com.example.smartcity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import butterknife.BindView;
 
 public class LogInFragment extends Fragment {
+
+    @BindView(R.id.welcomeText)
+    TextView welcomeText;
+
+    @BindView(R.id.mailInput)
+    EditText mailInput;
+
+    @BindView(R.id.passwordInput)
+    EditText passwordInput;
+
+    @BindView(R.id.logInButton)
+    Button logingButton;
+
+    @BindView(R.id.registerButton)
+    Button registerButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login,container,false);
+        View v = inflater.inflate(R.layout.fragment_login,container,false);
+        this.registerButton = v.findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(registerListerner);
+        return v;
     }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private View.OnClickListener loginListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // actions à faire quand on clique sur le bouton login
+        }
+    };
+
+    private View.OnClickListener registerListerner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new CheckFragment());
+            transaction.commit();
+        }
+    };
 }
